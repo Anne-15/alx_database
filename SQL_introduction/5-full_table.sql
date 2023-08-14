088a02583eccca8ddd359b90a308f843381cdf5c
Anne-15 <wariiyuanne@gmail.com>
2023-08-14 15:41:33 +0300

    changes

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,4 +2,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 $*/
-SHOW COLUMNS FROM first_table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
