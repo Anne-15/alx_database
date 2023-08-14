b64842d26e8f9e15e71740412706c5e50de86c8d
Anne-15 <wariiyuanne@gmail.com>
2023-08-14 13:12:57 +0300

    Listing all rows

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -3,7 +3,5 @@
 -- because Batch 3 is the best!
 $*/
     
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+SELECT description 
+FROM first_table;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+/*$ cat my_script.sql
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+$*/
+
+SHOW ROWS FROM first_table
