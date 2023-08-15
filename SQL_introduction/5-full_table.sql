ac70110791c6019638b076849df372aeff74e1a9
Anne-15 <wariiyuanne@gmail.com>
2023-08-15 12:23:50 +0300

    making changes

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,6 +2,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 $*/
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+USE DATABASE hbtn_0c_0
+SHOW CREATE TABLE first_table
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- because Batch 3 is the best!
 $*/
 CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL ON *.* TO 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
