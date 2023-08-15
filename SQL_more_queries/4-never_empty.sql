9ccc4d5d1c48d7accd35e344072c86c3da6f4979
Anne-15 <wariiyuanne@gmail.com>
2023-08-15 11:31:56 +0300

    CHANGES

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+/*$ cat my_script.sql
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+$*/
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT PRIMARY KEY
+    name VARCHAR(256)
+)
