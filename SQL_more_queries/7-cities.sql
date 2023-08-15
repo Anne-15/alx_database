33e9f21d17699966122d252bcd9f37f67e30a8d5
Anne-15 <wariiyuanne@gmail.com>
2023-08-15 12:02:43 +0300

    foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+/*$ cat my_script.sql
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+$*/
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id)
+    name VARCHAR(256) NOT NULL
+)
