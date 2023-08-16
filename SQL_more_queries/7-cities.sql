02885345676f599a6255ca30a738409e63409cc3
Anne-15 <wariiyuanne@gmail.com>
2023-08-16 21:23:31 +0300

    changes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,8 +3,9 @@
 -- because Batch 3 is the best!
 $*/
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
