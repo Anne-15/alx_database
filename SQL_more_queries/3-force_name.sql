e1f0540bbcec968446689fca21196403c1f2ba03
Anne-15 <wariiyuanne@gmail.com>
2023-08-16 21:02:59 +0300

    changes

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -2,7 +2,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 $*/
-CREATE TABLE IF NOT EXISTS force_name(
+CREATE TABLE force_name(
     id INT
     name VARCHAR(256) NOT NULL
 )
