f46ec13a545597553c79970260983f22792672a2
Anne-15 <wariiyuanne@gmail.com>
2023-08-16 21:19:06 +0300

    making changes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 $*/
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
