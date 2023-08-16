976d76492afd924d0af3022ce4cf337800202f09
Anne-15 <wariiyuanne@gmail.com>
2023-08-16 21:22:45 +0300

    changing

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,7 +3,8 @@
 -- because Batch 3 is the best!
 $*/
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
